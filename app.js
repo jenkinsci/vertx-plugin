f9dfec9160c42d6d7b430b19cefdc5f7c291e934
Brian Lalor <blalor@bravo5.org>
2012-09-06 21:28:40 -0600

    unregister handlers on stop, schedule build on start

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
 load('vertx.js');
 
-function handler(msg) {
-    console.log(JSON.stringify(msg));
+function hndl1(msg) {
+    console.log("jenkins-vertx -- " + JSON.stringify(msg));
 }
 
-vertx.eventBus.registerHandler("jenkins-vertx", handler);
-vertx.eventBus.registerHandler("jenkins.items", handler);
-vertx.eventBus.registerHandler("jenkins.run", handler);
+function hndl2(msg) {
+    console.log("jenkins.item -- " + JSON.stringify(msg));
+}
+
+function hndl3(msg) {
+    console.log("jenkins.run -- " + JSON.stringify(msg));
+}
+
+var hndl1_id = vertx.eventBus.registerHandler("jenkins-vertx", hndl1);
+var hndl2_id = vertx.eventBus.registerHandler("jenkins.item", hndl2);
+var hndl3_id = vertx.eventBus.registerHandler("jenkins.run", hndl3);
+
+function vertxStop() {
+    console.log("stopping");
+
+    vertx.eventBus.unregisterHandler(hndl1_id, hndl1);
+    vertx.eventBus.unregisterHandler(hndl2_id, hndl2);
+    vertx.eventBus.unregisterHandler(hndl3_id, hndl3);
+}
 
 console.log("ready");
+
+vertx.eventBus.send(
+    "jenkins",
+    {
+        "action": "scheduleBuild",
+        "data": {
+            "projectName": "parameterized",
+            "params": {
+                "key":"vert.x value",
+                "foo":"bar"
+            }
+        }
+    },
+    function(r) {
+        console.log(JSON.stringify(r));
+    }
+);
+
